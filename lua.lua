9fb247c5f7055f3f60d3f5a008f181e464ba7f72
xCLY-Dev <72878688+xCLY-Dev@users.noreply.github.com>
2021-02-06 23:07:42 +0300

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,14 +1,9 @@
--- Gui to Lua
--- Version: 3.2
-
--- Instances:
 
 local Loader31sj = Instance.new("ScreenGui")
 local emcan = Instance.new("ImageLabel")
 local TextLabel = Instance.new("TextLabel")
 local Frame = Instance.new("Frame")
 
---Properties:
 
 Loader31sj.Name = "Loader31sj"
 Loader31sj.Parent = game.CoreGui
@@ -20,7 +15,7 @@ emcan.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 emcan.BackgroundTransparency = 1.000
 emcan.ClipsDescendants = true
 emcan.Position = UDim2.new(0.499497503, 0, 0.5, 0)
-emcan.Size = UDim2.new(0, 65, 0, 0)
+emcan.Size = UDim2.new(0, 68, 0, 0)
 emcan.Image = "rbxassetid://3570695787"
 emcan.ImageColor3 = Color3.fromRGB(33, 33, 33)
 emcan.ScaleType = Enum.ScaleType.Slice
@@ -34,7 +29,7 @@ TextLabel.ClipsDescendants = true
 TextLabel.Position = UDim2.new(0.212643683, 0, 0.25, 0)
 TextLabel.Size = UDim2.new(0, 200, 0, 50)
 TextLabel.Font = Enum.Font.GothamSemibold
-TextLabel.Text = "Loading"
+TextLabel.Text = "Text Function"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextSize = 25.000
 
@@ -47,15 +42,15 @@ Frame.Size = UDim2.new(0, 12, 0, 2)
 
 -- Scripts:
 
-local function JPDNTQL_fake_script() -- Frame.LocalScript 
+local function IBXS_fake_script() -- Frame.LocalScript 
 	local script = Instance.new('LocalScript', Frame)
 
-	local ld = 7
-	local wt = 0.5
+	local ld = 0.01
+	local wt = 0.01
 	local wd = 0.5
 	local text = script.Parent.Parent.TextLabel
 	local bar = script.Parent.Parent.Frame
-	
+	local a = false
 	
 	function textt(t)
 		text.Text = t
@@ -65,23 +60,44 @@ local function JPDNTQL_fake_script() -- Frame.LocalScript
 		wait(t)
 	end
 	--{0, 348},{0, 100}
+	wait(1)
 	script.Parent.Parent.Parent.emcan:TweenSize(UDim2.new(0,348,0,100),"Out","Sine",wd)
-	w(1)
-	script.Parent.Parent.Frame:TweenSize(UDim2.new(0,318,0,2),"Out","Sine",ld)
+	wait(1)
+	script.Parent.Parent.Frame:TweenSize(UDim2.new(0,59,0,2),"Out","Sine",0.5)
 	textt("Loading")
+	a = true
+	wait(1)
+	if a == true then 
+	a = false
+	script.Parent.Parent.Frame:TweenSize(UDim2.new(0,102,0,2),"Out","Sine",0.5) 
+		textt("Checking Game")
+		a = true
+	end
 	w(1)
-	textt("Checking Game")
-	w(1)
-	textt("Checking Host")
-	w(1)
-	textt("Executing Script")
-	w(1)
-	textt("Enjoy :)")
-	text.Visible = false
-	script.Parent.Parent.Parent.emcan:TweenSize(UDim2.new(0,65,0,0),"Out","Sine",wt)
+	if a == true then 
+		a = false
+		script.Parent.Parent.Frame:TweenSize(UDim2.new(0,200,0,2),"Out","Sine",0.5) 
+		textt("Checking Host")
+		a = true
+	end
+	wait(1)
+	if a == true then 
+		a = false
+		script.Parent.Parent.Frame:TweenSize(UDim2.new(0,250,0,2),"Out","Sine",0.5) 
+		textt("Executing Script")
+		a = true
+	end
+	wait(1)
+	if a == true then 
+		a = false
+		script.Parent.Parent.Frame:TweenSize(UDim2.new(0,300,0,2),"Out","Sine",0.5) 
+		textt("Enjoy :)")
+		a = true
+	end
+	wait(1)
+	script.Parent.Parent.Parent.emcan:TweenSize(UDim2.new(0,0,0,0),"Out","Sine",wd)
+	wait(1)
+	script.Parent.Parent.Parent.Parent.Loader31sj:Destroy()
 	
-	--{0, 65},{0, 0}
 end
-coroutine.wrap(JPDNTQL_fake_script)()
-
-wait(6)
+coroutine.wrap(IBXS_fake_script)()
