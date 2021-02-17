3a6a5c5f1bf524b56bf385767c2f364122c87798
xCLY-Dev <72878688+xCLY-Dev@users.noreply.github.com>
2021-02-17 12:21:16 +0300

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,49 +1,80 @@
+-- Gui to Lua
+-- Version: 3.2
 
-local Loader31sj = Instance.new("ScreenGui")
-local emcan = Instance.new("ImageLabel")
-local TextLabel = Instance.new("TextLabel")
+-- Instances:
+
+local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
+local Frame_2 = Instance.new("Frame")
+local ImageLabel = Instance.new("ImageLabel")
+local Frame_3 = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
 
+--Properties:
 
-Loader31sj.Name = "Loader31sj"
-Loader31sj.Parent = game.CoreGui
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-emcan.Name = "emcan"
-emcan.Parent = Loader31sj
-emcan.AnchorPoint = Vector2.new(0.5, 0.5)
-emcan.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-emcan.BackgroundTransparency = 1.000
-emcan.ClipsDescendants = true
-emcan.Position = UDim2.new(0.499497503, 0, 0.5, 0)
-emcan.Size = UDim2.new(0, 68, 0, 0)
-emcan.Image = "rbxassetid://3570695787"
-emcan.ImageColor3 = Color3.fromRGB(33, 33, 33)
-emcan.ScaleType = Enum.ScaleType.Slice
-emcan.SliceCenter = Rect.new(100, 100, 100, 100)
-emcan.SliceScale = 0.040
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.ClipsDescendants = true
+Frame.Position = UDim2.new(0.344554454, 0, 0.413087904, 0)
+Frame.Size = UDim2.new(0, 315, 0, 85)
 
-TextLabel.Parent = emcan
+Frame_2.Parent = Frame
+Frame_2.AnchorPoint = Vector2.new(0.5, 0.5)
+Frame_2.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+Frame_2.BorderSizePixel = 0
+Frame_2.ClipsDescendants = true
+Frame_2.Position = UDim2.new(0.498412699, 0, 0.513683379, 0)
+Frame_2.Size = UDim2.new(0, 10, 0, 2)
+
+ImageLabel.Parent = Frame_2
+ImageLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BackgroundTransparency = 1.000
+ImageLabel.ClipsDescendants = true
+ImageLabel.Position = UDim2.new(0.49701494, 0, 0.495599657, 0)
+ImageLabel.Size = UDim2.new(0, 11, 0, 9)
+ImageLabel.Image = "rbxassetid://3570695787"
+ImageLabel.ImageColor3 = Color3.fromRGB(15, 15, 15)
+
+Frame_3.Parent = ImageLabel
+Frame_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame_3.BorderSizePixel = 0
+Frame_3.Position = UDim2.new(0.219367027, 0, 0.563370824, 2)
+Frame_3.Size = UDim2.new(0, 12, 0, 1)
+Frame_3.Visible = false
+
+TextLabel.Parent = ImageLabel
 TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.BackgroundTransparency = 1.000
-TextLabel.ClipsDescendants = true
-TextLabel.Position = UDim2.new(0.212643683, 0, 0.25, 0)
+TextLabel.LayoutOrder = -3
+TextLabel.Position = UDim2.new(0.312825739, 0, 0.447257787, 2)
 TextLabel.Size = UDim2.new(0, 200, 0, 50)
 TextLabel.Font = Enum.Font.GothamSemibold
-TextLabel.Text = "Text Function"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.TextSize = 25.000
-
-Frame.Parent = emcan
-Frame.BackgroundColor3 = Color3.fromRGB(123, 180, 255)
-Frame.BorderSizePixel = 0
-Frame.ClipsDescendants = true
-Frame.Position = UDim2.new(0, 15, 0.819999993, 0)
-Frame.Size = UDim2.new(0, 12, 0, 2)
+TextLabel.TextSize = 28.000
+TextLabel.TextTransparency = 1.000
 
 -- Scripts:
 
-local function IBXS_fake_script() -- Frame.LocalScript 
-	local script = Instance.new('LocalScript', Frame)
+local function CXEQQHC_fake_script() -- ImageLabel.LocalScript 
+	local script = Instance.new('LocalScript', ImageLabel)
+
+	local gui = script.Parent
+	local ts = game:GetService("TweenService")
+	
+	local goal = {}
+	wait(1)
+	local tween = gui:TweenSize(UDim2.new(0, 534,0, 507), Enum.EasingDirection.In,Enum.EasingStyle.Sine,0.5,true)
+	
+		--{0, 25},{0, 23}
+end
+coroutine.wrap(CXEQQHC_fake_script)()
+local function SEQCF_fake_script() -- Frame_3.LocalScript 
+	local script = Instance.new('LocalScript', Frame_3)
 
 	local ld = 0.01
 	local wt = 0.01
@@ -60,16 +91,15 @@ local function IBXS_fake_script() -- Frame.LocalScript
 		wait(t)
 	end
 	--{0, 348},{0, 100}
-	wait(1)
-	script.Parent.Parent.Parent.emcan:TweenSize(UDim2.new(0,348,0,100),"Out","Sine",wd)
-	wait(1)
+	wait(3)
+	script.Parent.Visible = true
 	script.Parent.Parent.Frame:TweenSize(UDim2.new(0,59,0,2),"Out","Sine",0.5)
 	textt("Loading")
 	a = true
 	wait(1)
 	if a == true then 
-	a = false
-	script.Parent.Parent.Frame:TweenSize(UDim2.new(0,102,0,2),"Out","Sine",0.5) 
+		a = false
+		script.Parent.Parent.Frame:TweenSize(UDim2.new(0,102,0,2),"Out","Sine",0.5) 
 		textt("Checking Game")
 		a = true
 	end
@@ -95,9 +125,78 @@ local function IBXS_fake_script() -- Frame.LocalScript
 		a = true
 	end
 	wait(1)
-	script.Parent.Parent.Parent.emcan:TweenSize(UDim2.new(0,0,0,0),"Out","Sine",wd)
-	wait(1)
-	script.Parent.Parent.Parent.Parent.Loader31sj:Destroy()
-	wait(10)
+	--[[
+	script.Parent.Parent.Parent.emcan:TweenSize(UDim2.new(0,348,0,100),"Out","Sine",wd)
+	w(1)
+	script.Parent.Parent.Frame:TweenSize(UDim2.new(0,318,0,2),"Out","Sine",ld)
+	textt("Loading")
+	w(1)
+	textt("Checking Game")
+	w(1)
+	textt("Checking Host")
+	w(1)
+	textt("Executing Script")
+	w(1)
+	textt("Enjoy :)")
+	text.Visible = false
+	script.Parent.Parent.Parent.emcan:TweenSize(UDim2.new(0,65,0,0),"Out","Sine",wt)
+	
+	--{0, 65},{0, 0}]]
 end
-coroutine.wrap(IBXS_fake_script)()
+coroutine.wrap(SEQCF_fake_script)()
+local function CZLQQ_fake_script() -- Frame_3.Rainbower 
+	local script = Instance.new('LocalScript', Frame_3)
+
+	while wait() do
+		script.Parent.BackgroundColor3 = Color3.new(1,0,0)
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.BackgroundColor3 = Color3.new(script.Parent.BackgroundColor3.r,script.Parent.BackgroundColor3.g+(17/255),script.Parent.BackgroundColor3.b)
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.BackgroundColor3 = Color3.new(script.Parent.BackgroundColor3.r-(17/255),script.Parent.BackgroundColor3.g,script.Parent.BackgroundColor3.b)
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.BackgroundColor3 = Color3.new(script.Parent.BackgroundColor3.r,script.Parent.BackgroundColor3.g,script.Parent.BackgroundColor3.b+(17/255))
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.BackgroundColor3 = Color3.new(script.Parent.BackgroundColor3.r,script.Parent.BackgroundColor3.g-(17/255),script.Parent.BackgroundColor3.b)
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.BackgroundColor3 = Color3.new(script.Parent.BackgroundColor3.r+(17/255),script.Parent.BackgroundColor3.g,script.Parent.BackgroundColor3.b)
+		end
+		for i=1,15 do
+			game:GetService("RunService").RenderStepped:wait()
+			script.Parent.BackgroundColor3 = Color3.new(script.Parent.BackgroundColor3.r,script.Parent.BackgroundColor3.g,script.Parent.BackgroundColor3.b-(17/255))
+		end
+	end
+end
+coroutine.wrap(CZLQQ_fake_script)()
+local function OXKSUXJ_fake_script() -- TextLabel.LocalScript 
+	local script = Instance.new('LocalScript', TextLabel)
+
+	local gui = script.Parent
+	wait(3)
+	
+	for i = 1,100 do
+		script.Parent.TextTransparency = script.Parent.TextTransparency - 0.05
+		wait()
+	end
+end
+coroutine.wrap(OXKSUXJ_fake_script)()
+local function KHEGNEV_fake_script() -- Frame_2.LocalScript 
+	local script = Instance.new('LocalScript', Frame_2)
+
+	local gui = script.Parent
+	wait(.1)
+	local tween = gui:TweenSize(UDim2.new(0, 329,0, 100), Enum.EasingDirection.In,Enum.EasingStyle.Sine,0.5,true)
+	wait(9)
+	local tween = gui:TweenSize(UDim2.new(0, 10,0, 2), Enum.EasingDirection.In,Enum.EasingStyle.Sine,0.5,true)
+	wait(1)
+	script.Parent.Parent.Parent.Parent.ScreenGui:Destroy()
+end
+coroutine.wrap(KHEGNEV_fake_script)()
