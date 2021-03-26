a52ad41d99c8e2efb06711f8bdd4256b2d5b51b6
xCLY-Dev <72878688+xCLY-Dev@users.noreply.github.com>
2021-03-26 14:40:39 +0300

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,3 +1,6 @@
+local GetName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId)
+_G.gamenamegay = GetName.Name
+
 local a = game:GetService("Players").LocalPlayer
 
 function Getmetatable()
@@ -10,176 +13,160 @@ function Drawingg()
    print("Drawing : true")
 end
 
+Drawingg()
+Getmetatable()
 local ScreenGui = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local Frame_2 = Instance.new("Frame")
+local Frame = Instance.new("ImageLabel")
+local BAR = Instance.new("ImageLabel")
+local TextButton = Instance.new("TextButton")
 local ImageLabel = Instance.new("ImageLabel")
-local Frame_3 = Instance.new("Frame")
-local TextLabel = Instance.new("TextLabel")
+local TEXT = Instance.new("ImageLabel")
+local gamelabel = Instance.new("TextLabel")
+
 
 ScreenGui.Parent = game.CoreGui
 
+Frame.Name = "Frame"
 Frame.Parent = ScreenGui
 Frame.AnchorPoint = Vector2.new(0.5, 0.5)
 Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Frame.BackgroundTransparency = 1.000
 Frame.ClipsDescendants = true
 Frame.Position = UDim2.new(0.5, 0, 0.5, 0)
-Frame.Size = UDim2.new(0, 315, 0, 85)
+Frame.Image = "rbxassetid://3570695787"
+Frame.ImageColor3 = Color3.fromRGB(25, 25, 25)
+Frame.ScaleType = Enum.ScaleType.Slice
+Frame.SliceCenter = Rect.new(100, 100, 100, 100)
+Frame.SliceScale = 0.080
 
-Frame_2.Parent = Frame
-Frame_2.AnchorPoint = Vector2.new(0.5, 0.5)
-Frame_2.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-Frame_2.BorderSizePixel = 0
-Frame_2.ClipsDescendants = true
-Frame_2.Position = UDim2.new(0.498412699, 0, 0.513683379, 0)
-Frame_2.Size = UDim2.new(0, 10, 0, 2)
+BAR.Name = "BAR"
+BAR.Parent = Frame
+BAR.AnchorPoint = Vector2.new(0.5, 0.5)
+BAR.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+BAR.BackgroundTransparency = 1.000
+BAR.ClipsDescendants = true
+BAR.Position = UDim2.new(0.5, 0, 0.0900000036, 0)
+BAR.Image = "rbxassetid://3570695787"
+BAR.ImageColor3 = Color3.fromRGB(20, 20, 20)
+BAR.ScaleType = Enum.ScaleType.Slice
+BAR.SliceCenter = Rect.new(100, 100, 100, 100)
+BAR.SliceScale = 0.080
 
-ImageLabel.Parent = Frame_2
-ImageLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+TextButton.Parent = BAR
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BackgroundTransparency = 1.000
+TextButton.Position = UDim2.new(0.886666656, 0, 0, 0)
+TextButton.Size = UDim2.new(0, 34, 0, 27)
+TextButton.Font = Enum.Font.Gotham
+TextButton.Text = "X"
+TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.TextSize = 20.000
+
+ImageLabel.Parent = Frame
 ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 ImageLabel.BackgroundTransparency = 1.000
-ImageLabel.ClipsDescendants = true
-ImageLabel.Position = UDim2.new(0.49701494, 0, 0.495599657, 0)
-ImageLabel.Size = UDim2.new(0, 11, 0, 9)
-ImageLabel.Image = "rbxassetid://3570695787"
-ImageLabel.ImageColor3 = Color3.fromRGB(15, 15, 15)
+ImageLabel.Position = UDim2.new(-0.00169354677, 0, -0.0481034294, 0)
+ImageLabel.Size = UDim2.new(0, 40, 0, 40)
+ImageLabel.Image = "http://www.roblox.com/asset/?id=6567126195"
 
-Frame_3.Parent = ImageLabel
-Frame_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Frame_3.BorderSizePixel = 0
-Frame_3.Position = UDim2.new(0.219367027, 0, 0.563370824, 2)
-Frame_3.Size = UDim2.new(0, 12, 0, 1)
-Frame_3.Visible = false
+TEXT.Name = "TEXT"
+TEXT.Parent = Frame
+TEXT.AnchorPoint = Vector2.new(0.5, 0.5)
+TEXT.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TEXT.BackgroundTransparency = 1.000
+TEXT.ClipsDescendants = true
+TEXT.Position = UDim2.new(0.498333335, 0, 0.562614977, 0)
+TEXT.Image = "rbxassetid://3570695787"
+TEXT.ImageColor3 = Color3.fromRGB(20, 20, 20)
+TEXT.ScaleType = Enum.ScaleType.Slice
+TEXT.SliceCenter = Rect.new(100, 100, 100, 100)
+TEXT.SliceScale = 0.080
 
-TextLabel.Parent = ImageLabel
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.LayoutOrder = -3
-TextLabel.Position = UDim2.new(0.312825739, 0, 0.447257787, 2)
-TextLabel.Size = UDim2.new(0, 200, 0, 50)
-TextLabel.Font = Enum.Font.Gotham
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.TextSize = 20.000
-TextLabel.TextTransparency = 1.000
+gamelabel.Name = "gamelabel"
+gamelabel.Parent = TEXT
+gamelabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+gamelabel.BackgroundTransparency = 1.000
+gamelabel.Position = UDim2.new(0, 0, 0.00849754736, 0)
+gamelabel.Font = Enum.Font.Gotham
+gamelabel.Text = _G.gamenamegay
+gamelabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+gamelabel.TextSize = 25.000
 
 -- Scripts:
 
-local function DIIPG_fake_script() -- ImageLabel.LocalScript 
+local function SNTO_fake_script() -- Frame.LocalScript 
+	local script = Instance.new('LocalScript', Frame)
+
+	wait(2)
+	local gui = script.Parent
+	local tween = gui:TweenSize(UDim2.new(0, 100,0, 100), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
+	wait(2)
+	local tween = gui:TweenSize(UDim2.new(0, 300,0, 150), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
+	wait(4)
+	local tween = gui:TweenSize(UDim2.new(0, 100,0, 100), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
+	wait(3)
+	local tween = gui:TweenSize(UDim2.new(0, 0,0, 0), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
+	
+end
+coroutine.wrap(SNTO_fake_script)()
+local function QOUX_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
+
+	local gui = script.Parent.Parent.Parent.Parent.Parent.ScreenGui
+	local but = script.Parent
+	but.MouseButton1Click:Connect(function()
+		gui:Destroy()
+	end)
+end
+coroutine.wrap(QOUX_fake_script)()
+local function ZBQZ_fake_script() -- BAR.LocalScript 
+	local script = Instance.new('LocalScript', BAR)
+
+	local gui = script.Parent
+	wait(4)
+	local tween = gui:TweenSize(UDim2.new(0, 300,0, 27), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
+	wait(4)
+	local tween = gui:TweenSize(UDim2.new(0, 0,0, 0), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
+	gui.Visible = false
+	
+end
+coroutine.wrap(ZBQZ_fake_script)()
+local function TSZDDPE_fake_script() -- ImageLabel.LocalScript 
 	local script = Instance.new('LocalScript', ImageLabel)
 
+	wait(2)
 	local gui = script.Parent
-	local ts = game:GetService("TweenService")
-	
-	local goal = {}
-	wait(1)
-	local tween = gui:TweenSize(UDim2.new(0, 534,0, 507), Enum.EasingDirection.In,Enum.EasingStyle.Sine,0.5,true)
-	
-		--{0, 25},{0, 23}
-end
-coroutine.wrap(DIIPG_fake_script)()
-local function WJTK_fake_script() -- Frame_3.LocalScript 
-	local script = Instance.new('LocalScript', Frame_3)
-
-	local ld = 0.01
-	local wt = 0.01
-	local wd = 0.5
-	local text = script.Parent.Parent.TextLabel
-	local bar = script.Parent.Parent.Frame
-	local a = false
-	
-	function textt(t)
-		text.Text = t
-	end
-	
-	function w(t)
-		wait(t)
-	end
-	--{0, 348},{0, 100}
+	local tween = gui:TweenSize(UDim2.new(0, 100,0, 100), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
+	wait(2)
+	local tween = gui:TweenSize(UDim2.new(0, 40,0, 40), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
+	wait(4)
+	local tween = gui:TweenSize(UDim2.new(0, 100,0, 100), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
 	wait(3)
-	script.Parent.Visible = true
-	script.Parent.Parent.Frame:TweenSize(UDim2.new(0,59,0,2),"Out","Sine",0.5)
-	textt("Loading...")
-	a = true
-	wait(1)
-	if a == true then 
-		a = false
-		script.Parent.Parent.Frame:TweenSize(UDim2.new(0,102,0,2),"Out","Sine",0.2) 
-		textt("Checking game...")
-		a = true
-	end
-	w(1)
-	if a == true then 
-		a = false
-		script.Parent.Parent.Frame:TweenSize(UDim2.new(0,150,0,2),"Out","Sine",0.2) 
-		textt("Getting data...")
-		w(1)
-		a = true
-	end
-	wait(1)
-	if a == true then 
-		a = false
-		script.Parent.Parent.Frame:TweenSize(UDim2.new(0,200,0,2),"Out","Sine",0.2) 
-		textt("Checking librarys..")
-		Drawingg()
-		Getmetatable()
-		a = true
-	end
-	wait(1)
-	if a == true then 
-		a = false
-		script.Parent.Parent.Frame:TweenSize(UDim2.new(0,250,0,2),"Out","Sine",0.2) 
-		textt("Getting ready..")
-		a = true
-	end
-	wait(1)
-	if a == true then 
-		a = false
-		script.Parent.Parent.Frame:TweenSize(UDim2.new(0,300,0,2),"Out","Sine",0.2) 
-		textt("Enjoy!")
-		a = true
-		wait(2)
-	end
-	--[[
-	script.Parent.Parent.Parent.emcan:TweenSize(UDim2.new(0,348,0,100),"Out","Sine",wd)
-	w(1)
-	script.Parent.Parent.Frame:TweenSize(UDim2.new(0,318,0,2),"Out","Sine",ld)
-	textt("Loading")
-	w(1)
-	textt("Checking Game")
-	w(1)
-	textt("Checking Host")
-	w(1)
-	textt("Executing Script")
-	w(1)
-	textt("Enjoy :)")
-	text.Visible = false
-	script.Parent.Parent.Parent.emcan:TweenSize(UDim2.new(0,65,0,0),"Out","Sine",wt)
+	local tween = gui:TweenSize(UDim2.new(0, 0,0, 0), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
 	
-	--{0, 65},{0, 0}]]
 end
-coroutine.wrap(WJTK_fake_script)()
-local function FGTUCAY_fake_script() -- TextLabel.LocalScript 
-	local script = Instance.new('LocalScript', TextLabel)
+coroutine.wrap(TSZDDPE_fake_script)()
+local function PWIJCZC_fake_script() -- TEXT.LocalScript 
+	local script = Instance.new('LocalScript', TEXT)
 
 	local gui = script.Parent
-	wait(3)
+	wait(4)
+	local tween = gui:TweenSize(UDim2.new(0, 270,0, 105), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
+	wait(4)
+	local tween = gui:TweenSize(UDim2.new(0, 0,0, 0), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
+	gui.Visible = false
 	
-	for i = 1,100 do
-		script.Parent.TextTransparency = script.Parent.TextTransparency - 0.05
-		wait()
-	end
+	--270 105
 end
-coroutine.wrap(FGTUCAY_fake_script)()
-local function WECK_fake_script() -- Frame_2.LocalScript 
-	local script = Instance.new('LocalScript', Frame_2)
+coroutine.wrap(PWIJCZC_fake_script)()
+local function YQOGKS_fake_script() -- gamelabel.LocalScript 
+	local script = Instance.new('LocalScript', gamelabel)
 
 	local gui = script.Parent
-	wait(.1)
-	local tween = gui:TweenSize(UDim2.new(0, 329,0, 100), Enum.EasingDirection.In,Enum.EasingStyle.Sine,0.5,true)
-	wait(11)
-	local tween = gui:TweenSize(UDim2.new(0, 0,0, 0), Enum.EasingDirection.In,Enum.EasingStyle.Sine,0.5,true)
-	wait(1)
-	script.Parent.Parent.Parent.Parent.ScreenGui:Destroy()
+	wait(4)
+	local tween = gui:TweenSize(UDim2.new(0, 270,0, 105), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
+	wait(4)
+	local tween = gui:TweenSize(UDim2.new(0, 0,0, 0), Enum.EasingDirection.In,Enum.EasingStyle.Quad,0.2,true)
+	gui.Visible = false
 end
-coroutine.wrap(WECK_fake_script)()
+coroutine.wrap(YQOGKS_fake_script)()
